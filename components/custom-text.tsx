f332ca1768fe8146ec03700e6156607f648b8189
abhishekbadole8 <abhishekbadole12@gmail.com>
2024-12-22 14:43:04 +0530

    custom text component created

---
+++ components/custom-text.tsx
@@ -0,0 +1,56 @@
+import { StyleProp, Text, TextProps, TextStyle } from "react-native";
+import React from "react";
+
+interface CustomTextProps extends TextProps {
+  fontSize?: number;
+  color?: string;
+  fontFamily?: string;
+  fontWeight?:
+    | "normal"
+    | "bold"
+    | "100"
+    | "200"
+    | "300"
+    | "400"
+    | "500"
+    | "600"
+    | "700"
+    | "800"
+    | "900";
+  textAlign?: "auto" | "left" | "right" | "center" | "justify";
+  lineHeight?: number;
+  style?: StyleProp<TextStyle>;
+}
+
+const CustomText: React.FC<CustomTextProps> = ({
+  children,
+  fontSize = 16,
+  color = "black",
+  fontFamily = "Gilroy-Medium",
+  fontWeight = "normal",
+  textAlign = "auto",
+  lineHeight,
+  style,
+  ...props
+}) => {
+  return (
+    <Text
+      style={[
+        {
+          fontSize,
+          color,
+          fontFamily,
+          fontWeight,
+          textAlign,
+          lineHeight: lineHeight ? lineHeight : fontSize * 1.2,
+        },
+        style,
+      ]}
+      {...props}
+    >
+      {children || ""}
+    </Text>
+  );
+};
+
+export default CustomText;
